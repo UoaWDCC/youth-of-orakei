08eca0908831fe6f9c4b7f1d425c0ffd61e19fe6
emmanuelaraba <emmanuelaraba77@gmail.com>
2024-06-01 23:11:51 +1200

    feat: started making new getDetails function"

--- src/scripts/getDetails.ts
+++ src/scripts/getDetails.ts
@@ -1,16 +1,37 @@
 import { Client } from "@notionhq/client";
 import * as fs from "fs";
 import { type block } from "../types/block";
+import type {BlockObjectRequest} from "@notionhq/client/build/src/api-endpoints";
 
-export async function getDetails(): Promise<any[]> {
-  // Check if the secret(s) are missing 
-  if (!import.meta.env.NOTION_TOKEN || !import.meta.env.NOTION_MEMBERS_ID) {
+//
+type MemberDate = {
+  team:string;
+  desc:string;
+  name:string;
+  cover?:string;
+  url?:string;
+}
+
+type Block = BlockObjectRequest;
+
+export async function getDetails(): Promise<void>{
+  const NOTION_TOKEN = import.meta.env.NOTION_TOKEN;
+  const NOTION_MEMBERS_ID = import.meta.env.NOTION_MEMBERS_ID;
+
+  if (!NOTION_TOKEN || !NOTION_MEMBERS_ID) {
     throw new Error("Missing secret(s)");
   }
 
-  // Create a new Notion client 
-  const notion = new Client({ auth: import.meta.env.NOTION_TOKEN });  
+  // initialising the Notion client
+  const notion = new Client({auth : NOTION_TOKEN});
 
-  return [];  
+  try {
+    // Query the Notion database
+    const query = await notion.databases.query({
+      database_id: NOTION_MEMBERS_ID,
+      sorts: [{ property: 'Name', direction: 'ascending' }]
+    });
 
-}
+    // iterating through all the pages in the database, and running the get
+
+  }
