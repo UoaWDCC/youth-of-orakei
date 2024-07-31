59c4832a5ded7caa23e6762c3804c62b7703df43
Anton Garay <antonvince.garay@gmail.com>
2024-07-31 17:44:03 +1200

    Added types to the ProjectTeam component.
    It wasn't importing the ProjectTeam component as no types were being declared. Also fixed the <a> tag inside of the button as it didn't take full width.

--- src/components/ProjectTeam.tsx
+++ src/components/ProjectTeam.tsx
@@ -3,17 +3,24 @@ import '../styles/global.css';
 import "../styles/members.css";
 // import MembersDisplay from "../components/MembersDisplay.astro";
 import PersonCard from "./PersonCard.tsx";
+import type { memberData } from "../types/memberData.ts";
 
-import { getMembers } from "../scripts/getMembers";
-let members = await getMembers();
+type TeamDetails = {
+  teamName: string;
+  description: string;
+  teamId: string;
+};
 
-import { sortMembersByTeam } from "../scripts/sortTeams.ts";
-let { leadershipTeam, communicationTeam, projects } =
-  sortMembersByTeam(members);
+type Project = {
+  teamDetails: TeamDetails;
+  members: memberData[];
+};
 
-console.log(projects);
+type ProjectTeamsProps = {
+  projects: Project[];
+};
 
-const ProjectTeams = () => {
+const ProjectTeams = ({ projects }: ProjectTeamsProps) => {
   const [selectedTeam, setSelectedTeam] = useState(projects[0].teamDetails.teamId); // Default state is team1
   const [teamNumber, setTeamNumber] = useState(0);
 
@@ -41,6 +48,7 @@ const ProjectTeams = () => {
           <p className="main-description-box-proj" style={{ padding: 0, margin: 0 }}>View the teams!</p>
           {projects.map((team) => (
             <button
+              key={team.teamDetails.teamId}
               className={team.teamDetails.teamId !== selectedTeam ? "project-button" : "project-button project-button-active"}
               onClick={() => handleTeamChange(team.teamDetails.teamId)}
             >
@@ -52,6 +60,7 @@ const ProjectTeams = () => {
       <div className="right1">
         {projects[teamNumber].members.map((member) => (
           <PersonCard
+            key={member.name}
             cover={member.cover}
             name={member.name}
             team={member.team}
@@ -59,7 +68,7 @@ const ProjectTeams = () => {
           />
         ))}
       </div>
-    </div >
+    </div>
   );
 };
 
---
+++ src/types/memberData.ts
@@ -0,0 +1,7 @@
+export type memberData = {
+    team: string;
+    desc: string;
+    name: string;
+    cover: string;
+    url?: string;
+  }
