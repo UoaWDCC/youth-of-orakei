7dedfab560211f05d3f05ab6cf7eef92943f18c9
Andrew Qiu <git@andrewqiu.com>
2024-08-03 16:40:51 +1200

    allow alt tags image components

--- src/components/index page/IndexImage.tsx
+++ src/components/index page/IndexImage.tsx
@@ -1,9 +1,16 @@
 import styles from './index.module.css'
 
-const IndexImage = ({source}: {source: string}) => {
+type props = {
+    source: string,
+    desc: string;
+}
+
+const IndexImage = ({source, desc}: props) => {
+    if(!desc) console.log("An image element is missing an alt tag, please add one!");
+
     return (
         <div className={styles.imageWrapper}>
-            <img className={styles.image} src={source}>
+            <img className={styles.image} src={source} alt={desc}>
 
             </img>
         </div>
