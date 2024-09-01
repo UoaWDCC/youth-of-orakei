246eb415ec1bdcf0dc008532a89e7e1b50fe8614
kimkimz <kimberleymzhu@gmail.com>
2024-09-01 15:14:31 +1200

    modified sortProjects script to sort project details by team

--- src/scripts/sortProjects.ts
+++ src/scripts/sortProjects.ts
@@ -6,37 +6,24 @@ type ProjectData = {
   url?: string;
 }
 
-type ProjectDetails = {
-  teamName: string;
-  description: string;
-  teamId: string;
-};
-
 export function sortMembersByTeam(projects: ProjectData[]) {
-  const leadershipTeam: ProjectData[] = [];
-  const communicationTeam: ProjectData[] = [];
-  const projectsMap: { [projectName: string]: { teamDetails: ProjectDetails; members: ProjectData[] } } = {};
-  let nextProjectId = 1; // Start ID counter at 0
+  const team1: ProjectData[] = [];
+  const team2: ProjectData[] = [];
+  const team3: ProjectData[] = [];
+  const team4: ProjectData[] = [];
 
   projects.forEach(project => {
-    if (project.team === "Leadership Team") {
-      leadershipTeam.push(project);
-    } else if (project.team === "Communication Team") {
-      communicationTeam.push(project);
-    } else if (project.team.startsWith("Projects:")) {
-      const projectName = project.team.replace("Projects: ", "");
-      if (!projectsMap[projectName]) {
-        projectsMap[projectName] = {
-          teamDetails: { teamName: projectName, description: project.desc, teamId: "team" + nextProjectId.toString() },
-          members: []
-        };
-        nextProjectId++;
-      }
-      projectsMap[projectName].members.push(project);
+    if (project.team === "Team 1") {
+      team1.push(project);
+    } else if (project.team === "Team 2") {
+      team2.push(project);
+    } else if (project.team === "Team 3") {
+      team3.push(project);
+    }
+    else if (project.team === "Team 4") {
+      team4.push(project);
     }
   });
 
-  const projectsTeam = Object.values(projectsMap);
-
-  return { leadershipTeam, communicationTeam, projectsTeam };
+  return { team1, team2, team3, team4 };
 }
