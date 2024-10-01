e7fca61a1c836b65c2d2165e7655db0000db75f2
Anton Garay <antonvince.garay@gmail.com>
2024-10-01 22:52:37 +1300

    try again

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient();
+const client = new PrismaClient({
+  log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
+})
 
-export default prisma;
+// global variable that stores the client
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? client
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = client
+
+prisma.member.findMany({})
