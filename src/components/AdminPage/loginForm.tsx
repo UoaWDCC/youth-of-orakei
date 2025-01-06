fda4986e4993d1d242d2c9ba4e1fff9666272d56
AntGa <antonvince.garay@gmail.com>
2025-01-06 15:14:46 +1300

    Refactor: Split notion download into four parts

--- src/components/AdminPage/loginForm.tsx
+++ src/components/AdminPage/loginForm.tsx
@@ -1,23 +1,34 @@
-import { useState } from 'react';
-import '../../styles/refresh.css';
-import RefreshSection from './RefreshSection';
+import { useState, useEffect } from "react";
+import "../../styles/refresh.css";
+import RefreshSection from "./RefreshSection";
 
 const LoginForm = () => {
-  const [errorMessage, setErrorMessage] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [password, setPassword] = useState("");
   const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Check for stored login state on mount
+    const storedIsLoggedIn = localStorage.getItem("isLoggedIn");
+    const storedPassword = localStorage.getItem("password");
+
+    if (storedIsLoggedIn === "true" && storedPassword) {
+      setLoggedIn(true);
+      setPassword(storedPassword);
+    }
+  }, []);
 
   const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
-    const enteredPassword = formData.get('password') as string;
+    const enteredPassword = formData.get("password") as string;
 
-    const response = await fetch('/api/refresh-login', {
-      method: 'POST',
-      body: JSON.stringify({ action: 'login', password: enteredPassword }),
+    const response = await fetch("/api/refresh-login", {
+      method: "POST",
+      body: JSON.stringify({ action: "login", password: enteredPassword }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
 
@@ -26,12 +37,15 @@ const LoginForm = () => {
       setPassword(enteredPassword); // Store the password after login
       setLoggedIn(true); // Hide login form and show "Logged in"
     } else {
-      setErrorMessage('Invalid password. Try again.');
+      setErrorMessage("Invalid password. Try again.");
     }
   };
 
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>, formId: string) => {
-    if (event.key === 'Enter') {
+  const handleKeyDown = (
+    event: React.KeyboardEvent<HTMLInputElement>,
+    formId: string
+  ) => {
+    if (event.key === "Enter") {
       event.preventDefault();
       const form = document.getElementById(formId) as HTMLFormElement;
       if (form) {
@@ -45,14 +59,16 @@ const LoginForm = () => {
       <div className="login-form-wrapper">
         {!loggedIn ? (
           <form id="login-form" onSubmit={handleLogin} className="login-form">
-            <label htmlFor="password" className='password-label'>Enter Password:</label>
+            <label htmlFor="password" className="password-label">
+              Enter Password:
+            </label>
             <input
               type="password"
               id="password"
               name="password"
               required
               onChange={(e) => setPassword(e.target.value)}
-              onKeyDown={(e) => handleKeyDown(e, 'login-form')}
+              onKeyDown={(e) => handleKeyDown(e, "login-form")}
               className="password-input"
             />
             <button type="submit" className="login-button">
